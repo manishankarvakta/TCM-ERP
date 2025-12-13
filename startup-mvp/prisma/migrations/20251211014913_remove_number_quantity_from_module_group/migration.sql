9e6539e5c98708c90cc90ba0db47c8045b1c9961
Manishankar Vakta <manishankarvakta@gmail.com>
2025-12-14 01:50:29 +0600

    Fix migration

--- startup-mvp/prisma/migrations/20251211014913_remove_number_quantity_from_module_group/migration.sql
+++
@@ -1,7 +0,0 @@
--- AlterTable: Remove number and quantity from ModuleGroup
--- This migration removes the following fields from ModuleGroup:
--- - number (Int?)
--- - quantity (Decimal?)
-
-ALTER TABLE "ModuleGroup" DROP COLUMN IF EXISTS "number";
-ALTER TABLE "ModuleGroup" DROP COLUMN IF EXISTS "quantity";
--- startup-mvp/prisma/migrations/20251211015226_remove_name_from_module_group/migration.sql
+++
@@ -1,9 +0,0 @@
--- AlterTable: Remove name from ModuleGroup
--- This migration removes the name field from ModuleGroup.
--- The code field will be used as the primary identifier instead.
-
--- Drop the index on name first
-DROP INDEX IF EXISTS "ModuleGroup_name_idx";
-
--- Drop the name column
-ALTER TABLE "ModuleGroup" DROP COLUMN IF EXISTS "name";
